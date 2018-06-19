96a81e010214c0fde3f855ca93158eb1dfa38c3d
xiaohuochai <121631835@qq.com>
2018-06-20 01:52:46 +0800

    增加离线缓存功能

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
+registerServiceWorker()
 ReactDOM.render(
   <Provider store={store}>
     <App />
